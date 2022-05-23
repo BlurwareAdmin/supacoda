07067a94561cb0e3155c79a41f651623d9890984
BlurwareAdmin <admin@blurware.com>
2022-05-23 17:02:44 +0000

    update

--- __main__.py
+++ __main__.py
@@ -40,8 +40,15 @@ credentials = pulumi.Output.all(resource_group.name, registry.name).apply(
 admin_username = credentials.username
 admin_password = credentials.passwords[0]["value"]
 
-custom_image = "supacoda"
-my_image =  docker.RemoteImage(custom_image, name=f"custom_image:latest")
+custom_image = "supa-coda"
+my_image = docker.Image(custom_image,
+    image_name=registry.login_server.apply(
+        lambda login_server: f"{login_server}/{custom_image}:latest"),
+    build=docker.DockerBuild(context=f"./{custom_image}"),
+    registry=docker.ImageRegistry(
+        server=registry.login_server,
+        username=admin_username,
+        password=admin_password))
 
 container_app = web.ContainerApp("app",
     resource_group_name=resource_group.name,
