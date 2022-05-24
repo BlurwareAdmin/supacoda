05289b9516b8bfab90e584ae8ff3e0f675c7f253
BlurwareAdmin <admin@blurware.com>
2022-05-24 21:54:52 +0000

    added function for container launch prep

--- ctz/ctz/app.py
+++ ctz/ctz/app.py
@@ -3,68 +3,111 @@
 CTZ is a developer tool for container development with python. 
 """
 
-import typer
+import os
 import subprocess
+
+#Third-Party Imports 
+from azure.identity._credentials.azure_cli import AzureCliCredential
 from azure.identity import DefaultAzureCredential
+import typer
+
 
 app = typer.Typer()
 
+creds = DefaultAzureCredential()
+
 @app.command(help="Start a new ctz project")
-def init(container_name: str, repo: str):
+def init(
+    container_name: str = "", 
+    template: str = typer.Argument(
+        "https://github.com/BlurwareAdmin/supacoda.git", 
+        envvar="GIT_TEMPLATE")):
     """
     Generate a new stamp python container. 
         ctz init [container_name] 
-        cd [container_name]
-        ctz build [container_name]
-        ctz publish 
     """
-    typer.echo(f"Hello {name}")
+   
+    subprocess.Popen(['git', 'clone', template])
+    subprocess.Popen(['mv', 'supacoda/', container_name])
+
+def login():
+    user_meta = subprocess.run(
+        [
+        'az',
+        'ad', 'signed-in-user', 'show', 
+        '--output', 'json', 
+        '--query', 'accountEnabled', 
+        '--only-show-errors'], 
+        capture_output=True, text=True
+    ) 
+    if user_meta.stdout is not None: 
+        typer.echo("You are already logged in!")
+    else: 
+        try: 
+            typer.echo("You will need to sign in to continue. Default is device login. For Trusted Local Development Only.")
+            subprocess.run( ['az', 'login', '--use-device-code'])
+        except: 
+            typer.echo('Login Failed.') 
 
 @app.command(help="Authenitcate with azure for developmet with ctz.")
 def az_auth():
     """
     Authenticate via a device code with wthe az cli. 
     """
-    if not service: 
-        if service: 
-            typer.echo("# Sign in with the AZ CLI. For development only.")
-            subprocess.run( ['az', 'login', '--use-device-code'])
-            credential = DefaultAzureCredential()
-            if credential: 
-                typer.echo("You have logged in")
+    try: 
+        login()
+    except Exception as e: 
+        return e
+
+def prep_containerapps():
+    login()
+    out = subprocess.run(
+        [
+        'az' , 'extension', 'add' , 
+        '--name', 'containerapp', 
+        '--upgrade']
+    )
+    out = subprocess.run(
+        [
+        'az' ,'provider' ,'register' ,
+        '--namespace' ,'Microsoft.OperationalInsights'
+        ]
+    )
+    check_env = [
+        'RESOURCE_GROUP', 'LOCATION', 'CONTAINERAPPS_ENVIRONMENT'
+    ]
+    checks = [
+        os.getenv(x) is not None for x in check_env
+    ]
+    if False in checks:
+        print(checks)
+        typer.Abort()
     else: 
-        typer.echo("Not yet supported")
-
-@app.command(help="Authenitcate with doppler for developmet with ctz.")
-def doppler_auth():
-    """
-    """
-     
-
-@app.command(help="Authenitcate with docker for developmet with ctz.")
-def docker_auth():
-    """
-    Authenticate with a secure token. 
-    """
-    if not service: 
-        if service: 
-            typer.echo("# Sign in with the AZ CLI. For development only.")
-            subprocess.run( ['az', 'login', '--use-device-code'])
-            credential = DefaultAzureCredential()
-            if credential: 
-                typer.echo("You have logged in")
-    else: 
-        typer.echo("Not yet supported")
-
+        out = subprocess.Popen([
+            'az', 'group',  'create' ,
+            '--name' ,f"{os.getenv('RESOURCE_GROUP')}", 
+            '--location' , f"{os.getenv('LOCATION')}"
+        ])
+        env = subprocess.Popen(
+        ['az', 'containerapp', 'env', 'create' ,
+            '--name' , f"{os.getenv('CONTAINERAPPS_ENVIRONMENT')}",
+            '--resource-group', f"{os.getenv('RESOURCE_GROUP')}", 
+            '--location', f"{os.getenv('LOCATION')}"
+        ])
+        print(out)
 
 @app.command(help="Deploy a container to docker hub and az container apps.")
-def deploy(container_repo_url: str, launch_location: str):
+def deploy(
+    container_repo_url: str = "" , 
+    launch_location: str = "westus"):
     """
     Deploy your image to Azure Container Apps. This uses az cli underhood so your must be authenticated. 
     Use the command `ctz az_auth` for authenitcation with a device. 
     For more information, see microsoft docs. https://docs.microsoft.com/en-us/azure/developer/python/sdk/authentication-overview
     """
-    typer.echo(f"Hello {name}")
+    
+    typer.echo(f"Deploy Container App with {container_repo_url}")
+    prep_containerapps()
 
 @app.command(help="Pull a container & run locally.")
 def run(container_repo_url: str):
